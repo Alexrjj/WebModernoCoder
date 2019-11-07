0a0b8c8d64347fd81bb030569d8db1e5bb1614a9
Alexrjj <alexsoares2517@gmail.com>
2019-11-06 21:43:55 -0300

    Tipo Number

---
+++ tipoNumberCuidados.js
@@ -0,0 +1,8 @@
+console.log(7 / 0) // Infinity
+console.log('10' / 2) /// É possível realizar cálculo com uma string + número.
+console.log('Show!' * 2)
+console.log(0.1 + 0.7)
+// console.log(10.toString()); <- não é possível
+console.log((10.345).toFixed(2))
+console.log('3' + 2) // String tem preferência. Concatena ao invés de somar.
+console.log('3' - 2) // Nesse caso, é realizado o cálculo como se fosse do tipo número.
---
+++ tipoNumberMath.js
@@ -0,0 +1,7 @@
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2)
+const elevaQuad = Math.pow(2, 8) // Math.pow trabalha com potenciação.
+
+console.log(area.toFixed(2))
+console.log(typeof Math)
+console.log(elevaQuad)
