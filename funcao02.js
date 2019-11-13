70415a25b72a0b912d392ecd7779e219142176b0
Alexrjj <alexsoares2517@gmail.com>
2019-11-13 20:59:45 -0300

    Trabalhando com função - parte 02

---
+++ funcao02.js
@@ -0,0 +1,19 @@
+//Armazenando uma função em uma variável
+const imprimirSoma = function (a, b) {
+  console.log(a + b)
+}
+imprimirSoma(2, 3)
+
+// Armazenando uma função arrow em uma variável
+const Subtracao = (a, b) => {
+  return a - b
+}
+console.log(Subtracao(2, 3))
+
+// Retorno implícito
+const divisao = (a, b) => a / b
+console.log(divisao(8, 4))
+
+// Ainda mais reduzido
+const imprimirValor = x => console.log(x)
+imprimirValor('Hello World!')
